3827093e92431eee45526540886de336f49e5334
N1ceL <41979846+N1ceL@users.noreply.github.com>
2021-03-10 00:11:42 +1000

    Add files via upload

---
+++ Includes.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "stdafx.h"
+#include <windows.h>
+#include <iostream> 
+#include <iomanip>
+#include <sstream>
+#include <fstream> 
+#include <msclr\marshal_cppstd.h> 
+#include <string>
+#include <direct.h>
+#using <System.dll>
+#include <sstream>
+#include <algorithm>
+#include <string>
+#include <algorithm>
+
+#include "WinUser.h"
+#pragma comment (lib, "user32.lib")
